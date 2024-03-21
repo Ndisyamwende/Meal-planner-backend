75b75d73093b378777ac61cfa89b182ff100a615
Alex Kimeu <alexkimeu1999@gmail.com>
2024-03-21 22:14:49 +0300

    Added exception and status codes

--- meal/main.py
+++ meal/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -14,7 +14,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/meal")
+@app.post("/meal", status_code=status.HTTP_201_CREATED)
 def create_meal(request: schemas.Meal, db: Session = Depends(get_db)):
     new_meal = models.Meal(image=request.image, title=request.title, ingredients=request.ingredients, recipe=request.recipe, nutrition=request.nutrition)
     db.add(new_meal)
@@ -28,6 +28,10 @@ def get_meals(db: Session = Depends(get_db)):
     return meals
 
 @app.get("/meal/{meal_id}")
-def get_meal(meal_id: int, db: Session = Depends(get_db)):
+def get_meal(meal_id: int,response: Response, db: Session = Depends(get_db), status_code=status.HTTP_200_OK):
     meal = db.query(models.Meal).filter(models.Meal.id == meal_id).first()
+    if not meal:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Meal with id {meal_id} not available")
+        # response.status_code=status.HTTP_404_NOT_FOUND
+        # return {"detail": f"Meal with id {meal_id} not available"}
     return meal
